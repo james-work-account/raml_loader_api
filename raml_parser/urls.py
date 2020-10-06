217a1282ec672dd53a557e6eb7ef5dfab4c8c873
James Whiteley <31282758+james-work-account@users.noreply.github.com>
2020-10-06 09:25:54 +0100

    added html output (#1)
    * added html output
    
    * removed requirements.txt because I don't know what I'm doing

--- raml_parser/urls.py
+++ raml_parser/urls.py
@@ -2,7 +2,7 @@ from django.urls import path
 
 from . import views
 
-app_name = 'polls'
+app_name = 'raml_loader_api'
 urlpatterns = [
-    path('<str:raml_name>/', views.get_documentation, name='vote'),
+    path('<str:raml_folder>/', views.get_documentation, name='get_documentation'),
 ]
--- raml_parser/views.py
+++ raml_parser/views.py
@@ -1,21 +1,36 @@
 from django.http import JsonResponse, HttpResponse
 import json
 import yaml
+import django
 from . import loader
+from django.shortcuts import render
+from yaml.scanner import ScannerError
 
 
-def get_documentation(request, raml_name):
+def get_documentation(request, raml_folder):
     try:
-        with open("raml/%s" % raml_name, 'r') as stream:
-            try:
-                y = yaml.load(stream, loader.Loader)
-                j = json.dumps(y)
-                return HttpResponse(status=200, content=j)
-            except yaml.scanner.ScannerError:
-                message = {"code": "PARSER_ERROR",
-                           "message": "Unable to parse %s" % raml_name}
-                return HttpResponse(status=400, content=json.dumps(message))
-            else:
-                return HttpResponse(status=500)
-    except FileNotFoundError:
+        j = json.loads(__fetch_documentation(request, raml_folder))
+        context = {
+            'title': j["title"],
+            'sections': j["sections"]
+        }
+        return render(request, 'raml_parser/index.html', context)
+    except ScannerError as ex:
+        return HttpResponse(status=400, content=ex.problem, content_type="application/json")
+    except FileNotFoundError as ex:
+        print(ex)
         return HttpResponse(status=204)
+    except NotADirectoryError:
+        return HttpResponse(status=204)
+
+
+def __fetch_documentation(request, raml_folder):
+    with open("raml/%s/application.raml" % raml_folder, 'r') as stream:
+        try:
+            y = yaml.load(stream, loader.Loader)
+            j = json.dumps(y)
+            return j
+        except ScannerError:
+            message = {"code": "PARSER_ERROR",
+                       "message": "Unable to parse %s" % raml_folder}
+            raise ScannerError(problem=json.dumps(message))
